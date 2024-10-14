f5889ad32fd4b2be921142ba69c8d5387ad3ac24
Dzianis Andreyeu <andninbox@gmail.com>
2024-10-15 00:10:25 +0300

    Add ExchangeDataReader.read_latest_trades and integration tests

--- data_reader.py
+++ data_reader.py
@@ -2,27 +2,44 @@ import os
 from select import select
 
 import ccxt.pro as ccxt
-from entities import TimeFrame, Exchange
+from entities import TimeFrame, Exchange, Trade
 
 
 class ExchangeDataReader:
     def __init__(self, exchange_config: Exchange):
         exchange_class = getattr(ccxt, exchange_config.id.value)
-        self._exchange = exchange_class({
-            'apiKey': os.environ.get("GP_API_KEY", exchange_config.api_key),
-            'secret': os.environ.get("GP_API_SECRET", exchange_config.api_secret),
-        })
+        self._exchange = exchange_class(
+            {
+                "apiKey": os.environ.get("GP_API_KEY", exchange_config.api_key),
+                "secret": os.environ.get("GP_API_SECRET", exchange_config.api_secret),
+            }
+        )
         self._exchange.set_sandbox_mode(exchange_config.test_mode)
 
     @property
     def exchange(self):
         return self._exchange
 
-    async def read_ohlcv_data(self, symbol: str, time_frame: TimeFrame, limit: int) -> list[list[float]]:
-        return await self._exchange.fetch_mark_ohlcv(symbol, timeframe=time_frame.value, limit=limit)
+    async def read_ohlcv_data(
+        self, symbol: str, time_frame: TimeFrame, limit: int
+    ) -> list[list[float]]:
+        return await self._exchange.fetch_mark_ohlcv(
+            symbol, timeframe=time_frame.value, limit=limit
+        )
 
-    async def read_latest_trades(self, symbol: str):
-        pass
+    async def read_latest_trades(self, symbol: str) -> list[Trade]:
+        trades = await self._exchange.watch_trades(symbol)
+        return list(
+            map(
+                lambda t: Trade(
+                    side=t["side"],
+                    price=t["price"],
+                    amount=t["amount"],
+                    timestamp=t["timestamp"],
+                ),
+                trades,
+            )
+        )
 
     async def close(self):
         await self._exchange.close()
--- tests/integration/test_excange_data_reader.py
+++ tests/integration/test_excange_data_reader.py
@@ -1,4 +1,6 @@
 import pytest
+from pycares import symbol
+
 from data_reader import ExchangeDataReader
 from entities import TimeFrame
 
@@ -11,3 +13,11 @@ async def test_read_ohlcv_data(data_reader: ExchangeDataReader):
     assert isinstance(result, list)
     assert len(result) == limit
     assert len(result[0]) == 6
+
+
+@pytest.mark.asyncio
+async def test_read_latest_trades(data_reader: ExchangeDataReader):
+    result = await data_reader.read_latest_trades("BTC/USDT")
+    assert result
+    assert result[0].price > 0
+    assert result[0].amount > 0
