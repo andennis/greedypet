6cbdf26da52339eb8b0bd76dec27397fef49c642
Dzianis Andreyeu <andninbox@gmail.com>
2024-10-18 01:10:50 +0300

    Prepare async functions to do trading jobs Run greedybot as async console app

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ import signal
 import sys
 import click
 import pathlib
+import asyncio
+import functools
 
-from gp_config import load_config
+from gp_config import load_config, GPConfig
 
 logger = logging.getLogger(__name__)
 LOG_LEVELS = list(map(logging.getLevelName, [logging.INFO, logging.DEBUG]))
@@ -41,14 +43,57 @@ def _load_config(file_name: str):
         return cfg
 
 
-def _signal_handler(sig, frame):
-    logger.info("Bot stopped")
-    sys.exit(0)
+_RUNNING_EVENT = asyncio.Event()
+_TERMINATION_EVENT = asyncio.Event()
 
 
-signal.signal(signal.SIGINT, _signal_handler)
-signal.signal(signal.SIGTERM, _signal_handler)
-signal.signal(signal.SIGQUIT, _signal_handler)
+def _is_running() -> bool:
+    return not _RUNNING_EVENT.is_set()
+
+
+async def _read_market_trades(config: GPConfig):
+    logger.info(f"Trades reading started")
+    while _is_running():
+        logger.info(f"Read trades")
+        await asyncio.sleep(1)
+    logger.info(f"Trades reading finished")
+
+
+async def _track_trade_signals(config: GPConfig):
+    logger.info(f"Trade signals tracking started")
+    while _is_running():
+        logger.info(f"Track trade signals")
+        await asyncio.sleep(1)
+    logger.info(f"Trade signals tracking finished")
+
+
+async def _make_market_trades(config: GPConfig):
+    logger.info(f"Trading started")
+    while _is_running():
+        logger.info(f"Make trading")
+        await asyncio.sleep(1)
+    logger.info(f"Trading finished")
+
+
+async def _signal_handler():
+    logger.info("Trading process is being stopped")
+    _RUNNING_EVENT.set()
+    await _TERMINATION_EVENT.wait()
+    logger.info("Trading process has been stopped gracefully")
+    asyncio.get_event_loop().stop()
+
+
+async def run_trades(config: GPConfig):
+    ev_loop = asyncio.get_running_loop()
+    for sig in [signal.SIGINT, signal.SIGTERM, signal.SIGQUIT]:
+        ev_loop.add_signal_handler(sig, lambda: asyncio.create_task(_signal_handler()))
+
+    await asyncio.gather(
+        _read_market_trades(config),
+        _track_trade_signals(config),
+        _make_market_trades(config),
+    )
+    _TERMINATION_EVENT.set()
 
 
 @click.command()
@@ -71,6 +116,7 @@ def main(config_file: str, name: str, log_level: str, log_dir: str):
     _configure_logging(log_dir, log_level)
     logger.info(f"Bot {name} started")
     config = load_config(config_file)
+    asyncio.run(run_trades(config))
 
 
 if __name__ == "__main__":
