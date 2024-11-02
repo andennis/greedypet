31ec0c0f81f87fd7f4d153cc2c035cee7099d399
Dzianis Andreyeu <andninbox@gmail.com>
2024-11-02 20:58:46 +0300

    Add test test_sleep_to_next_timeframe

--- market_data_analyzer.py
+++ market_data_analyzer.py
@@ -14,10 +14,15 @@ class MarketDataAnalyzer:
     @cached_property
     def min_timeframe(self) -> TimeFrame:
         filters = self._config.entry_condition.filters
+        min_tf1 = min(map(lambda x: x.time_frame, filters), key=timeframe_to_sec)
         if self._config.exit_condition.mode == ExitMode.SIGNAL:
-            filters.extend(self._config.exit_condition.signal.filters)
+            min_tf2 = min(
+                map(lambda x: x.time_frame, self._config.exit_condition.signal.filters),
+                key=timeframe_to_sec,
+            )
+            min_tf1 = min(min_tf1, min_tf2, key=timeframe_to_sec)
 
-        return min(map(lambda x: x.time_frame, filters), key=timeframe_to_sec)
+        return min_tf1
 
     async def sleep_to_next_timeframe(self):
         tf = timeframe_to_sec(self.min_timeframe)
--- tests/unit/test_market_data_analyzer.py
+++ tests/unit/test_market_data_analyzer.py
@@ -1,5 +1,5 @@
+from unittest.mock import patch, PropertyMock, Mock
 import pytest
-
 from entities import DealConfig, TimeFrame
 from market_data_analyzer import MarketDataAnalyzer
 from tests.unit.conftest import trades_storage
@@ -31,7 +31,7 @@ _deal_config = {
                 },
             ],
         },
-    }
+    },
 }
 
 
@@ -45,16 +45,49 @@ def data_analyzer(deal_config, trades_storage):
     return MarketDataAnalyzer(deal_config, trades_storage)
 
 
-@pytest.mark.parametrize("time_frame, result", [
-    (TimeFrame.TF_5M, TimeFrame.TF_5M),
-    (TimeFrame.TF_1M, TimeFrame.TF_1M),
-    (TimeFrame.TF_1H, TimeFrame.TF_15M),
-])
-def test_min_timeframe(deal_config, data_analyzer, time_frame, result):
+@pytest.mark.parametrize(
+    "time_frame, result",
+    [
+        (TimeFrame.TF_5M, TimeFrame.TF_5M),
+        (TimeFrame.TF_1M, TimeFrame.TF_1M),
+        (TimeFrame.TF_1H, TimeFrame.TF_15M),
+    ],
+)
+def test_min_timeframe(
+    deal_config, data_analyzer, time_frame: TimeFrame, result: TimeFrame
+):
     deal_config.entry_condition.filters[0].time_frame = time_frame
     deal_config.entry_condition.filters[2].time_frame = time_frame
     assert data_analyzer.min_timeframe == result
 
 
-def test_sleep_to_next_timeframe():
-    raise NotImplementedError()
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "cur_time, time_frame, sleep_time", [
+        (600, TimeFrame.TF_1M, 60),
+        (601, TimeFrame.TF_1M, 59),
+        (659, TimeFrame.TF_1M, 1),
+        (600, TimeFrame.TF_5M, 5 * 60),
+        (600 + 150, TimeFrame.TF_5M, 5 * 60 - 150),
+        (3600, TimeFrame.TF_15M, 15 * 60),
+        (3600 + 600, TimeFrame.TF_15M, 15 * 60 - 600),
+    ]
+)
+@patch(
+    "market_data_analyzer.MarketDataAnalyzer.min_timeframe", new_callable=PropertyMock
+)
+@patch("market_data_analyzer.asyncio.sleep")
+@patch("market_data_analyzer.time.time")
+async def test_sleep_to_next_timeframe(
+    mock_time,
+    mock_sleep,
+    mock_min_timeframe,
+    data_analyzer,
+    cur_time: int,
+    time_frame: TimeFrame,
+    sleep_time: int,
+):
+    mock_min_timeframe.return_value = time_frame
+    mock_time.return_value = cur_time
+    await data_analyzer.sleep_to_next_timeframe()
+    mock_sleep.assert_awaited_once_with(sleep_time)
